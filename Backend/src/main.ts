4c7136b554e92cea2141eab78462a2553af2b48b
Faruk Valjevac <faruk.valjevac@at.bosch.com>
2025-06-12 13:05:56 +0200

    Makeup

--- Backend/src/main.ts
+++ Backend/src/main.ts
@@ -6,8 +6,6 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
     origin: 'http://localhost:5173',
-    methods: 'POST',
-    credentials: true,
   });
   await app.listen(parseInt(process.env.PORT as string, 10));
 }
